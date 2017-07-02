40361bfd2699ecbd508b9afa871ce038533998de
Hugo Puhlmann <hugopuhlmann@gmail.com>
2017-07-02 02:21:17 -0300

    Adding "/meme" command, relying on memegen.link.

--- src/main/kotlin/com/hstefan/fotcbot/FotcBot.kt
+++ src/main/kotlin/com/hstefan/fotcbot/FotcBot.kt
@@ -1,6 +1,9 @@
 package com.hstefan.fotcbot
 
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
 import org.telegram.telegrambots.api.methods.send.SendMessage
+import org.telegram.telegrambots.api.methods.send.SendPhoto
 import org.telegram.telegrambots.api.methods.updatingmessages.DeleteMessage
 import org.telegram.telegrambots.api.objects.Message
 import org.telegram.telegrambots.api.objects.Update
@@ -8,11 +11,17 @@ import org.telegram.telegrambots.api.objects.inlinequery.InlineQuery
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.exceptions.TelegramApiException
 import org.telegram.telegrambots.logging.BotLogger
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.net.URLEncoder
 import java.util.regex.Pattern
+import javax.imageio.ImageIO
 
 
 class FotcBot(private val config: Config) : TelegramLongPollingBot() {
     private val commandPattern = Pattern.compile("""/(\w*)@?\w*\s*(.*)$""")
+    private val argPattern = Pattern.compile("([^\"]\\S*|\".+?\")\\s*")
 
     override fun getBotUsername() = config.name
 
@@ -34,6 +43,57 @@ class FotcBot(private val config: Config) : TelegramLongPollingBot() {
         when (matcher.group(1)) {
             "me" -> handleMeCommand(message, args)
             "greet" -> handleGreetCommand(message)
+            "meme" -> handleMemeCommand(message, args)
+        }
+    }
+
+    private fun handleMemeCommand(message: Message, args: String) {
+        val argList = ArrayList<String>()
+
+        val matcher = argPattern.matcher(args)
+        while (matcher.find())
+            argList.add(matcher.group(1))
+
+        when (argList.size) {
+            3 -> makeMemeTopAndBottom(message, argList[0], argList[1], argList[2])
+            2 -> makeMemeTopOnly(message, argList[0], argList[1])
+            else ->
+                replyWithErrorMessage(message, "At least two parameters must be given to /meme.")
+        }
+    }
+
+    private fun makeMemeTopOnly(message: Message, memeId: String, topText: String) {
+        makeMemeTopAndBottom(message, memeId, topText, "_")
+    }
+
+    private fun makeMemeTopAndBottom(message: Message, memeId: String, topText: String,
+                                     bottomText: String) {
+        val memegenStr = { s: String -> s.trim('\"').replace(' ', '_')}
+
+        val idEnc = URLEncoder.encode(memegenStr(memeId), "UTF-8")
+        val topEnc = URLEncoder.encode(memegenStr(topText), "UTF-8")
+        val bottomEnc = URLEncoder.encode(memegenStr(bottomText), "UTF-8")
+
+        val httpClient = HttpClients.createDefault()
+        val httpGet = HttpGet("https://memegen.link/%s/%s/%s.jpg".format(idEnc, topEnc, bottomEnc))
+        val httpResp = httpClient.execute(httpGet)
+
+        try {
+            val image = ImageIO.read(httpResp.entity.content)
+            val os = ByteArrayOutputStream()
+            ImageIO.write(image, "jpeg", os)
+            val memePhoto = SendPhoto()
+            memePhoto.chatId = message.chatId.toString()
+            memePhoto.setNewPhoto("meme", ByteArrayInputStream(os.toByteArray()))
+            sendPhoto(memePhoto)
+        } catch (connEx: IOException) {
+            BotLogger.error(config.logTag, connEx)
+            replyWithErrorMessage(message, connEx.message.toString())
+        } catch (telegramEx: TelegramApiException) {
+            BotLogger.error(config.logTag, telegramEx)
+            replyWithErrorMessage(message, telegramEx.message.toString())
+        } finally {
+            httpResp.close()
         }
     }
 
